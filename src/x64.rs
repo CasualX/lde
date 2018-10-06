308de3eb51b09e34a80957234a33ea77dc05ee1a
Casper <CasualX@users.noreply.github.com>
2018-10-06 18:04:13 +0200

    Detailed InstLen (#7)

--- src/x64.rs
+++ src/x64.rs
@@ -8,6 +8,7 @@ May contain errors...
 */
 
 use contains::Contains;
+use InstLen;
 
 static TABLE_PREFIX: [u32; 8] = [
 	/* 0 1 2 3 4 5 6 7 8 9 A B C D E F 0 1 2 3 4 5 6 7 8 9 A B C D E F */
@@ -96,7 +97,7 @@ static TABLE_INVALID_C: [u32; 2] = [
 ];
 //---- Three-byte opcodes 3A ----
 
-pub fn lde_int(opcode: &[u8]) -> u32 {
+pub fn inst_len(opcode: &[u8]) -> InstLen {
 	let modrm;
 	let mut op: u8;
 	let (mut ddef, mut mdef) = (4u32, 8u32);
@@ -104,9 +105,14 @@ pub fn lde_int(opcode: &[u8]) -> u32 {
 	let mut it = opcode.iter();
 
 	// Prefixes
+	let mut prefix_len = 0;
 	loop {
-		op = if let Some(&op) = it.next() { op } else { return 0; };
+		op = match it.next() {
+			Some(&op) => op,
+			None => return InstLen::EMPTY,
+		};
 		if TABLE_PREFIX.has(op) {
+			prefix_len += 1;
 			// Operand-size override prefix
 			if op == 0x66 { ddef = 2u32; }
 			// Address-size override prefix
@@ -117,20 +123,33 @@ pub fn lde_int(opcode: &[u8]) -> u32 {
 		}
 	}
 
+	let mut op_len = 1;
 	if op == 0x0F {
-		op = if let Some(&op) = it.next() { op } else { return 0; };
+		op = match it.next() {
+			Some(&op) => op,
+			None => return InstLen::EMPTY,
+		};
+		op_len += 1;
 		// Three-byte opcodes (C)
 		if op == 0x38 {
-			op = if let Some(&op) = it.next() { op } else { return 0; };
+			op = match it.next() {
+				Some(&op) => op,
+				None => return InstLen::EMPTY,
+			};
+			op_len += 1;
 			// Invalid opcodes
-			if if op < 0x40 { TABLE_INVALID_C.has(op) } else { !((0x40..0x42).has(op) || (0x80..0x82).has(op) || (0xF0..0xF2).has(op)) } { return 0; };
+			if if op < 0x40 { TABLE_INVALID_C.has(op) } else { !((0x40..0x42).has(op) || (0x80..0x82).has(op) || (0xF0..0xF2).has(op)) } { return InstLen::EMPTY; };
 			modrm = true;
 		}
 		// Three-byte opcodes (D)
 		else if op == 0x3A {
-			op = if let Some(&op) = it.next() { op } else { return 0; };
+			op = match it.next() {
+				Some(&op) => op,
+				None => return InstLen::EMPTY,
+			};
+			op_len += 1;
 			// Invalid opcodes
-			if !((0x08..0x10).has(op) || (0x14..0x18).has(op) || (0x20..0x23).has(op) || (0x40..0x43).has(op) || (0x60..0x64).has(op)) { return 0; };
+			if !((0x08..0x10).has(op) || (0x14..0x18).has(op) || (0x20..0x23).has(op) || (0x40..0x43).has(op) || (0x60..0x64).has(op)) { return InstLen::EMPTY; };
 			modrm = true;
 			dsize += 1;
 		}
@@ -138,7 +157,7 @@ pub fn lde_int(opcode: &[u8]) -> u32 {
 		else {
 			// Invalid opcodes
 			if TABLE_INVALID_B.has(op) {
-				return 0;
+				return InstLen::EMPTY;
 			}
 			modrm = TABLE_MODRM_B.has(op);
 			// Check for imm8
@@ -155,11 +174,11 @@ pub fn lde_int(opcode: &[u8]) -> u32 {
 	else {
 		// Reject invalid opcodes
 		if TABLE_INVALID_A.has(op) {
-			return 0;
+			return InstLen::EMPTY;
 		}
 		modrm = TABLE_MODRM_A.has(op);
 		// Check `test` opcode with immediate
-		if (op == 0xF6 || op == 0xF7) && (if let Some(&op) = it.clone().next() { op } else { return 0; } & 0x38) == 0  {
+		if (op == 0xF6 || op == 0xF7) && (if let Some(&op) = it.clone().next() { op } else { return InstLen::EMPTY; } & 0x38) == 0 {
 			dsize += if (op & 1) != 0 { ddef } else { 1 }
 		}
 		// Check for imm8
@@ -182,13 +201,19 @@ pub fn lde_int(opcode: &[u8]) -> u32 {
 
 	// Mod R/M
 	if modrm {
-		op = if let Some(&op) = it.next() { op } else { return 0; };
+		op = match it.next() {
+			Some(&op) => op,
+			None => return InstLen::EMPTY,
+		};
 		let mode = op & 0xC0;
 		let rm = op & 0b111;
 		if mode != 0xC0 {
 			if rm == 0b100 {
 				// Scaled Index Byte
-				op = if let Some(&op) = it.next() { op } else { return 0; };
+				op = match it.next() {
+					Some(&op) => op,
+					None => return InstLen::EMPTY,
+				};
 				if mode == 0x00 {
 					if (op & 0b111) == 0b101 {
 						msize += 4;
@@ -210,39 +235,47 @@ pub fn lde_int(opcode: &[u8]) -> u32 {
 	}
 
 	// Get total length and bounds check
-	let mut total = ((it.as_slice().as_ptr() as usize).wrapping_sub(opcode.as_ptr() as usize)) as u32;
-	total = total.wrapping_add(dsize + msize);
-	if total as usize <= opcode.len() { total } else { 0 }
+	let total_len = ((it.as_slice().as_ptr() as usize).wrapping_sub(opcode.as_ptr() as usize)) as u32;
+	let total_len = total_len.wrapping_add(dsize + msize) as u8;
+
+	let arg_len = total_len - prefix_len - op_len;
+	if total_len as usize <= opcode.len() {
+		InstLen { total_len, op_len, arg_len, prefix_len }
+	}
+	else {
+		InstLen::EMPTY
+	}
 }
 
 //----------------------------------------------------------------
 
 #[cfg(test)]
-mod tests {
-	use super::lde_int;
-	#[test]
-	fn units() {
-		// sub rsp, *
-		assert_eq!(lde_int(b"\x48\x83\xEC*"), 4);
-		// lea rcx, [rip+****]
-		assert_eq!(lde_int(b"\x48\x8D\x0D****"), 7);
-		// rex push rbp
-		assert_eq!(lde_int(b"\x40\x55"), 2);
-		// movabs rax, ********
-		assert_eq!(lde_int(b"\x48\xA3********"), 10);
-		// addr32 mov ds:****, eax
-		assert_eq!(lde_int(b"\x67\x48\xA3****"), 7);
-		// mov qword ptr [rbp+****], ****
-		assert_eq!(lde_int(b"\x48\xC7\x85\xA8\x00\x00\x00\x0C\x00\x00\x00"), 11);
-		// mov word ptr [rsp+*], **
-		assert_eq!(lde_int(b"\x66\xC7\x44\x24\x34\x63\x74"), 7);
-		// nop dword ptr [rax+*]
-		assert_eq!(lde_int(b"\x0F\x1F\x40\x00"), 4);
-		// nop dword ptr [rax+****]
-		assert_eq!(lde_int(b"\x66\x0F\x0D\x80****"), 8);
-		// weird nop
-		assert_eq!(lde_int(b"\x66\x66\x0f\x1f\x84\x00\x00\x00\x00\x00"), 10);
-		// rep movsb
-		assert_eq!(lde_int(b"\xF3\xA4"), 2);
-	}
+fn lde_int(bytes: &[u8]) -> u32 {
+	inst_len(bytes).total_len as u32
+}
+
+#[test]
+fn units() {
+	// sub rsp, *
+	assert_eq!(lde_int(b"\x48\x83\xEC*"), 4);
+	// lea rcx, [rip+****]
+	assert_eq!(lde_int(b"\x48\x8D\x0D****"), 7);
+	// rex push rbp
+	assert_eq!(lde_int(b"\x40\x55"), 2);
+	// movabs rax, ********
+	assert_eq!(lde_int(b"\x48\xA3********"), 10);
+	// addr32 mov ds:****, eax
+	assert_eq!(lde_int(b"\x67\x48\xA3****"), 7);
+	// mov qword ptr [rbp+****], ****
+	assert_eq!(lde_int(b"\x48\xC7\x85\xA8\x00\x00\x00\x0C\x00\x00\x00"), 11);
+	// mov word ptr [rsp+*], **
+	assert_eq!(lde_int(b"\x66\xC7\x44\x24\x34\x63\x74"), 7);
+	// nop dword ptr [rax+*]
+	assert_eq!(lde_int(b"\x0F\x1F\x40\x00"), 4);
+	// nop dword ptr [rax+****]
+	assert_eq!(lde_int(b"\x66\x0F\x0D\x80****"), 8);
+	// weird nop
+	assert_eq!(lde_int(b"\x66\x66\x0f\x1f\x84\x00\x00\x00\x00\x00"), 10);
+	// rep movsb
+	assert_eq!(lde_int(b"\xF3\xA4"), 2);
 }
--- src/x86.rs
+++ src/x86.rs
@@ -10,6 +10,7 @@ May contain errors...
 */
 
 use contains::Contains;
+use InstLen;
 
 static TABLE_PREFIX: [u32; 8] = [
 	/* 0 1 2 3 4 5 6 7 8 9 A B C D E F 0 1 2 3 4 5 6 7 8 9 A B C D E F */
@@ -87,7 +88,7 @@ static TABLE_INVALID_C: [u32; 2] = [
 ];
 //---- Three-byte opcodes 3A ----
 
-pub fn lde_int(opcode: &[u8]) -> u32 {
+pub fn inst_len(opcode: &[u8]) -> InstLen {
 	let modrm;
 	let mut op: u8;
 	let (mut ddef, mut mdef) = (4u32, 4u32);
@@ -95,9 +96,14 @@ pub fn lde_int(opcode: &[u8]) -> u32 {
 	let mut it = opcode.iter();
 
 	// Prefixes
+	let mut prefix_len = 0;
 	loop {
-		op = if let Some(&op) = it.next() { op } else { return 0; };
+		op = match it.next() {
+			Some(&op) => op,
+			None => return InstLen::EMPTY,
+		};
 		if TABLE_PREFIX.has(op) {
+			prefix_len += 1;
 			// Operand-size override prefix
 			if op == 0x66 { ddef = 2u32; }
 			// Address-size override prefix
@@ -108,20 +114,33 @@ pub fn lde_int(opcode: &[u8]) -> u32 {
 		}
 	}
 
+	let mut op_len = 1;
 	if op == 0x0F {
-		op = if let Some(&op) = it.next() { op } else { return 0; };
+		op = match it.next() {
+			Some(&op) => op,
+			None => return InstLen::EMPTY,
+		};
+		op_len += 1;
 		// Three-byte opcodes (C)
 		if op == 0x38 {
-			op = if let Some(&op) = it.next() { op } else { return 0; };
+			op = match it.next() {
+				Some(&op) => op,
+				None => return InstLen::EMPTY,
+			};
+			op_len += 1;
 			// Invalid opcodes
-			if if op < 0x40 { TABLE_INVALID_C.has(op) } else { !((0x40..0x42).has(op) || (0x80..0x82).has(op) || (0xF0..0xF2).has(op)) } { return 0; };
+			if if op < 0x40 { TABLE_INVALID_C.has(op) } else { !((0x40..0x42).has(op) || (0x80..0x82).has(op) || (0xF0..0xF2).has(op)) } { return InstLen::EMPTY; };
 			modrm = true;
 		}
 		// Three-byte opcodes (D)
 		else if op == 0x3A {
-			op = if let Some(&op) = it.next() { op } else { return 0; };
+			op = match it.next() {
+				Some(&op) => op,
+				None => return InstLen::EMPTY,
+			};
+			op_len += 1;
 			// Invalid opcodes
-			if !((0x08..0x10).has(op) || (0x14..0x18).has(op) || (0x20..0x23).has(op) || (0x40..0x43).has(op) || (0x60..0x64).has(op)) { return 0; };
+			if !((0x08..0x10).has(op) || (0x14..0x18).has(op) || (0x20..0x23).has(op) || (0x40..0x43).has(op) || (0x60..0x64).has(op)) { return InstLen::EMPTY; };
 			modrm = true;
 			dsize += 1;
 		}
@@ -129,7 +148,7 @@ pub fn lde_int(opcode: &[u8]) -> u32 {
 		else {
 			// Invalid opcodes
 			if TABLE_INVALID_B.has(op) {
-				return 0;
+				return InstLen::EMPTY;
 			}
 			modrm = TABLE_MODRM_B.has(op);
 			// Check for imm8
@@ -146,7 +165,7 @@ pub fn lde_int(opcode: &[u8]) -> u32 {
 	else {
 		modrm = TABLE_MODRM_A.has(op);
 		// Check `test` opcode with immediate
-		if (op == 0xF6 || op == 0xF7) && (if let Some(&op) = it.clone().next() { op } else { return 0; } & 0x38) == 0  {
+		if (op == 0xF6 || op == 0xF7) && (if let Some(&op) = it.clone().next() { op } else { return InstLen::EMPTY; } & 0x38) == 0 {
 			dsize += if (op & 1) != 0 { ddef } else { 1 }
 		}
 		// Check for imm8
@@ -169,13 +188,19 @@ pub fn lde_int(opcode: &[u8]) -> u32 {
 
 	// Mod R/M
 	if modrm {
-		op = if let Some(&op) = it.next() { op } else { return 0; };
+		op = match it.next() {
+			Some(&op) => op,
+			None => return InstLen::EMPTY,
+		};
 		let mode = op & 0xC0;
 		let rm = op & 0b111;
 		if mode != 0xC0 {
 			if rm == 0b100 {
 				// Scaled Index Byte
-				op = if let Some(&op) = it.next() { op } else { return 0; };
+				op = match it.next() {
+					Some(&op) => op,
+					None => return InstLen::EMPTY,
+				};
 				if mode == 0x00 {
 					if (op & 0b111) == 0b101 {
 						msize += 4;
@@ -197,45 +222,53 @@ pub fn lde_int(opcode: &[u8]) -> u32 {
 	}
 
 	// Get total length and bounds check
-	let mut total = ((it.as_slice().as_ptr() as usize).wrapping_sub(opcode.as_ptr() as usize)) as u32;
-	total = total.wrapping_add(dsize + msize);
-	if total as usize <= opcode.len() { total } else { 0 }
+	let total_len = ((it.as_slice().as_ptr() as usize).wrapping_sub(opcode.as_ptr() as usize)) as u32;
+	let total_len = total_len.wrapping_add(dsize + msize) as u8;
+
+	let arg_len = total_len - prefix_len - op_len;
+	if total_len as usize <= opcode.len() {
+		InstLen { total_len, op_len, arg_len, prefix_len }
+	}
+	else {
+		InstLen::EMPTY
+	}
 }
 
 //----------------------------------------------------------------
 
 #[cfg(test)]
-mod tests {
-	use super::lde_int;
-	#[test]
-	fn units() {
-		// add al, *
-		assert_eq!(lde_int(b"\x04*"), 2);
-		// mov DWORD PTR [ebp+*]
-		assert_eq!(lde_int(b"\x89\x5D*"), 3);
-		// test al, al
-		assert_eq!(lde_int(b"\x84\xC0"), 2);
-		// fld QWORD PTR [eax+eax*4+****]
-		assert_eq!(lde_int(b"\xDD\x84\x00****"), 7);
-		// mov esi, ****
-		assert_eq!(lde_int(b"\xBE****"), 5);
-		// mov eax, fs:****
-		assert_eq!(lde_int(b"\x64\xA1****"), 6);
-		// add DWORD PTR ds:****, eax
-		assert_eq!(lde_int(b"\x01\x05****"), 6);
-		// addr16 mov eax, dx:**
-		assert_eq!(lde_int(b"\x67\xA1**"), 4);
-		// add BYTE PTR [bx+si+**], al
-		assert_eq!(lde_int(b"\x67\x00\x80**"), 5);
-		// inc eax
-		assert_eq!(lde_int(b"\x40"), 1);
-		// retn
-		assert_eq!(lde_int(b"\xC3"), 1);
-		// nop dword ptr [rax+*]
-		assert_eq!(lde_int(b"\x0F\x1F\x40\x00"), 4);
-		// nop dword ptr [rax+****]
-		assert_eq!(lde_int(b"\x66\x0F\x0D\x80****"), 8);
-		// clflush byte ptr [rax]
-		assert_eq!(lde_int(b"\x0F\xAE\x38"), 3);
-	}
+fn lde_int(bytes: &[u8]) -> u32 {
+	inst_len(bytes).total_len as u32
+}
+
+#[test]
+fn units() {
+	// add al, *
+	assert_eq!(lde_int(b"\x04*"), 2);
+	// mov DWORD PTR [ebp+*]
+	assert_eq!(lde_int(b"\x89\x5D*"), 3);
+	// test al, al
+	assert_eq!(lde_int(b"\x84\xC0"), 2);
+	// fld QWORD PTR [eax+eax*4+****]
+	assert_eq!(lde_int(b"\xDD\x84\x00****"), 7);
+	// mov esi, ****
+	assert_eq!(lde_int(b"\xBE****"), 5);
+	// mov eax, fs:****
+	assert_eq!(lde_int(b"\x64\xA1****"), 6);
+	// add DWORD PTR ds:****, eax
+	assert_eq!(lde_int(b"\x01\x05****"), 6);
+	// addr16 mov eax, dx:**
+	assert_eq!(lde_int(b"\x67\xA1**"), 4);
+	// add BYTE PTR [bx+si+**], al
+	assert_eq!(lde_int(b"\x67\x00\x80**"), 5);
+	// inc eax
+	assert_eq!(lde_int(b"\x40"), 1);
+	// retn
+	assert_eq!(lde_int(b"\xC3"), 1);
+	// nop dword ptr [rax+*]
+	assert_eq!(lde_int(b"\x0F\x1F\x40\x00"), 4);
+	// nop dword ptr [rax+****]
+	assert_eq!(lde_int(b"\x66\x0F\x0D\x80****"), 8);
+	// clflush byte ptr [rax]
+	assert_eq!(lde_int(b"\x0F\xAE\x38"), 3);
 }
